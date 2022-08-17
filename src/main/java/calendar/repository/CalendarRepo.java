befeec753081465eda6cf5e61085b91543fabb28
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2022-08-17 19:28:23 +0300

    create Apis

--- src/main/java/calendar/repository/CalendarRepo.java
+++ src/main/java/calendar/repository/CalendarRepo.java
@@ -10,5 +10,9 @@ import java.util.List;
 public interface CalendarRepo extends JpaRepository<Calendar, Long> {
     List<Calendar> findByUser_id(Long userId);
 
+    List<Calendar> findFirst3ByUser_id(Long userId);
+
     List<Calendar> findByType_common(boolean common);
+
+    List<Calendar> findByUser_idAndType_common(Long userId, boolean common);
 }
